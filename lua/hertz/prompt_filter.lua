194bf6dcc90605f7a1cd7de3492b246bc590b901
Bennett S. Owen <hertz@26hz.com.cn>
2024-12-23 22:51:45 +0800

    add schema name prompt

---
+++ lua/hertz/prompt_filter.lua
@@ -0,0 +1,21 @@
+-- prompt_filter.lua
+-- encoding: utf-8
+-- CC-BY-4.0
+
+local function F(input, env)
+    schema_name = env.engine.schema.config:get_string("schema/name")
+    local ctx = env.engine.context
+    local segment = ctx.composition:back()
+    
+    if segment then
+        segment.prompt = "    [ " .. schema_name .. " ]"
+        -- 保持原有的候选项不变
+        for cand in input:iter() do
+            yield(cand)
+        end
+    end
+    
+    return true
+end
+
+return F
